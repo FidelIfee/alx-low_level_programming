2dc87f5f33d8bd5c74ae19b84c27b5c0f6c4c1ab
FidelIfee <ifeeojiaku@gmail.com>
2022-10-18 14:33:01 -0700

    print_alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,22 +2,14 @@
 
 /**
  * print_alphabet_x10 - Make alphabet x10 times
- *
- * Return: void
  */
 
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	char letter;
 
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
-		i++;
-	}
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+		_putchar(letter);
+
+	_putchar("\n");
 }
