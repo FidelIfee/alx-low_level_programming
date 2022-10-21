e9801e6b57ba8916179b91257b5dd22e7c7ea3c7
FidelIfee <ifeeojiaku@gmail.com>
2022-10-21 16:18:04 -0700

    print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,27 +2,27 @@
 
 /**
  * print_triangle - create triangle
- * @size: params
+ * @size: parameter
  * Return: 0
  */
 
 void print_triangle(int size)
 {
-	int i = 0, j, n = size - 1;
+	int a = 0, b, n = size - 1;
 
 	if (size > 0)
 	{
-		for (; i < size; i++)
+		for (; a < size; a++)
 		{
-			for (j = 0; j < size; j++)
+			for (b = 0; b < size; b++)
 			{
-				if (j < n)
+				if (b < n)
 					_putchar(' ');
 				else
 					_putchar('#');
 			}
 			n--;
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 	else
