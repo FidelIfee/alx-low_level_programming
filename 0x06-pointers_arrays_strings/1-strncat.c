c5c1e6c205171e913f62ca1df893a0aa790bc3c4
FidelIfee <ifeeojiaku@gmail.com>
2022-10-27 13:41:07 -0700

    concatenates two strings 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,8 +10,8 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int length, a;
-/* a is a counter for  n bytes of src to be concatenated */
+	int length, j;
+/* j is a counter for  n bytes of src to be concatenated */
 /* length = length of destination string */
 
 	length = 0;
@@ -19,9 +19,9 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		length++;
 	}
-	for (a = 0; j < n && src[a] != '\0'; a++, length++)
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
 	{
-		dest[length] = src[a];
+		dest[length] = src[j];
 	}
 	dest[length] = '\0';
 	return (dest);
